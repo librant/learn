ccbc93f35f98b17c3776b09259584e3b12df26bd
shangfengli <shangfengli@tencent.com>
2022-10-21 20:04:47 +0800

    add valueOf

--- golang/gopkg/reflect/valueof/valueof-01/main.go
+++ golang/gopkg/reflect/valueof/valueof-01/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
 )
@@ -14,4 +15,9 @@ func main() {
 	var getA int = valueOfA.Interface().(int)
 	var getB int = int(valueOfA.Int())
 	fmt.Printf("getA = %d getB = %d\n", getA, getB)
+
+	err := errors.New("error")
+	valueOfE := reflect.ValueOf(err)
+	fmt.Printf("valueOfE: %v\n", valueOfE.IsNil())
+
 }
