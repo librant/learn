84269998e89cf0b64e9b20f4aa7cb8fb032fe4fa
shangfengli <shangfengli@tencent.com>
2022-11-08 09:46:55 +0800

    add slice

---
+++ golang/gopkg/slice/slice.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	// slice 变量本身的地址不会变, slice 中的 array 指针会因为 cap 容量不够后会重新分配；
+	a := make([]string, 0, 100)
+	log.Printf("[1]&a = %p", &a) // a 变量底层对应的是数组，切片 a 的地址不随切片的长度变动而变动；
+	log.Printf("[1]a = %p", a)   // a 的值对应底层数组的值，在 cap 容量内是不变的，超过 cap 时，由 append 改变
+	for i := 0; i < 100; i++ {
+		a = append(a, "I do not know ")
+	}
+	log.Printf("[2]&a = %p", &a)
+	log.Printf("[2]a = %p", a)
+	for i := 0; i < 1; i++ {
+		a = append(a, "I do not know ")
+	}
+	log.Printf("[3]&a = %p", &a)
+	log.Printf("[3]a = %p", a)
+	return
+}
