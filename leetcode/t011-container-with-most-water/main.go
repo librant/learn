3005f80931ef4f6f4fa21ba0d5c20e71b2e4b406
shangfengli <shangfengli@tencent.com>
2022-11-23 00:19:10 +0800

    leetcode-t011

--- leetcode/t011-container-with-most-water/main.go
+++ leetcode/t011-container-with-most-water/main.go
@@ -1,18 +1,43 @@
 package main
 
+import "fmt"
+
 // 给定一个长度为 n 的整数数组 height
 // 有 n 条垂线，第 i 条线的两个端点(i, 0)和(i, height[i])。
 // 找出其中的两条线，使得它们与x轴共同构成的容器可以容纳最多的水。
 
 func main() {
+	arr := []int{1,8,6,2,5,4,8,3,7}
 
+	fmt.Printf("max=%d", maxArea(arr))
 }
 
+// maxArea 双指针方式
 func maxArea(height []int) int {
 	n := len(height)
+	i, j := 0, n-1
+	res := 0
+	for j > i {
+		if height[j] > height[i] {
+			res = MAX(res, height[i] * (j-i))
+			i++
+		} else {
+			res = MAX(res, height[j] * (j-i))
+			j--
+		}
+	}
+	return res
+}
 
-	// 采用双指针的方式，当哪边的水少，就移动哪边的指针
-	maxWater := MIN(height[0], height[n-1]) * (n-1)
+// maxAreaTimeout 暴力求解，超出时间范围
+func maxAreaTimeout(height []int) int {
+	n := len(height)
+	maxWater := 0
+	for i := 0; i < n; i++ {
+		for j := n - 1; j > i; j-- {
+			maxWater = MAX(maxWater, MIN(height[i], height[j]) * (j - i))
+		}
+	}
 	return maxWater
 }
 
