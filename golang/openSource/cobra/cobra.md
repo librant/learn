1be4703f9bf78b622842c466c493aeaf2409009f
shangfengli <shangfengli@tencent.com>
2022-11-23 08:23:53 +0800

    delete same cobra

--- golang/openSource/cobra/cobra.md
+++
@@ -1,3 +0,0 @@
-
-cobra 常用实例：
-https://github.com/spf13/cobra/blob/main/projects_using_cobra.md
--- golang/openSource/cobra/demon/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/librant/learn/golang/openSource/cobra/demon/cmd"
-)
-
-func main() {
-	cmd.Execute()
-}
