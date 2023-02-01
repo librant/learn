5137975ffce245b6bddfa4e8cff1686dd44dde83
librant <lishangfeng@baidu.com>
2023-02-01 16:22:14 +0800

    add urfave-cli test-02

--- open-source/06-urfave-cli/urfave-cli-01/main.go
+++ open-source/06-urfave-cli/urfave-cli-01/main.go
@@ -22,6 +22,26 @@ func main() {
 		return nil
 	}
 
+	oApp.Commands = []cli.Command{
+		{
+			Name: "lang", 				// 命令全称
+			Aliases: []string{"l"},		// 命令简称
+			Usage:"Setting language",	// 命令详细描述
+			Action: func(c *cli.Context) {
+				// 通过c.Args().First()获取命令行参数
+				fmt.Printf("language=%v \n",c.Args().First())
+			},
+		},
+		{
+			Name: "encode",
+			Aliases: []string{"e"},
+			Usage:"Setting encoding",
+			Action: func(c *cli.Context) {
+				fmt.Printf("encoding=%v \n",c.Args().First())
+			},
+		},
+	}
+
 	// 命令执行
 	if err := oApp.Run(os.Args); err != nil {
 		log.Fatal(err)
